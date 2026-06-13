1add2cff6c4a8c9c059f6cab7b3045a235f9f88f
Mario <marion.uytter@gmail.com>
2026-06-13 02:39:50 +0200

    test railway minimal server

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const express = require("express");
 
 const app = express();
 
+// ✅ IMPORTANT : fallback port
+const PORT = process.env.PORT || 3000;
+
 app.get("/", (req, res) => {
     res.send("OK RAILWAY WORKING");
 });
-
-const PORT = process.env.PORT;
 
 app.listen(PORT, "0.0.0.0", () => {
     console.log("SERVER STARTED ON PORT:", PORT);
